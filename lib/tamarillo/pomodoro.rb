107872a29324d051096c702880f334c5d1e71ed2
Tim Uruski <timuruski@gmail.com>
2012-04-15 20:06:42 -0600

    Add states to Pomodoro model

--- lib/tamarillo/pomodoro.rb
+++ lib/tamarillo/pomodoro.rb
@@ -1,17 +1,39 @@
+require 'date'
+
 module Tamarillo
   class Pomodoro
-    def initialize(duration, started_at = nil)
-      @duration = duration
-      @started_at = (started_at || Time.now).to_i
+    module States
+      ACTIVE = :active.freeze
+      COMPLETED = :completed.freeze
+      INTERRUPTED = :interrupted.freeze
     end
 
-    def duration
-      @duration
+    attr_reader :duration, :started_at, :date
+
+    def initialize(duration, started_at = nil)
+      @duration = duration
+      @started_at = started_at || Time.now
+      @date = @started_at.to_date
     end
 
     def remaining
-      d = (@started_at + @duration) - Time.now.to_i
+      d = (@started_at.to_i + @duration) - Time.now.to_i
       d > 0 ? d : 0
     end
+
+    def interrupt!
+      @interrupted = true
+    end
+
+    def state
+      if @interrupted
+        States::INTERRUPTED
+      elsif remaining == 0
+        States::COMPLETED
+      else
+        States::ACTIVE
+      end
+    end
+
   end
 end
--- spec/lib/tamarillo/pomodoro_spec.rb
+++ spec/lib/tamarillo/pomodoro_spec.rb
@@ -2,45 +2,71 @@ require 'active_support/core_ext/numeric/time'
 require 'timecop'
 
 require_relative '../../../lib/tamarillo/pomodoro'
+include Tamarillo
 
 describe Tamarillo::Pomodoro do
+  before :all do
+    Timecop.freeze
+  end
 
-  subject { Tamarillo::Pomodoro.new(25.minutes) }
+  after :all do
+    Timecop.return
+  end
 
-  its(:duration) { should == 25.minutes }
+  subject { Pomodoro.new(25.minutes) }
+
+  describe "basic attibutes" do
+    its(:duration) { should == 25.minutes }
+    its(:started_at) { should == Time.now }
+    its(:date) { should == Date.today }
+    its(:state) { should == Pomodoro::States::ACTIVE }
+  end
 
   describe "remaining time" do
-    before :all do
-      Timecop.freeze
-    end
+    let!(:pomodoro) { Pomodoro.new(25.minutes) }
 
-    after :all do
-      Timecop.return
-    end
-
-    let!(:pomodoro) { Tamarillo::Pomodoro.new(25.minutes) }
-
-    it "can calculate the remainging time" do
+    it "works if no time has elapsed" do
       pomodoro.remaining.should == 25.minutes
     end
 
-    it "can calculate the remainging time" do
-      Timecop.travel(Time.now + 10.minutes)
+    it "works if time has elapsed" do
+      Timecop.travel(10.minutes)
       pomodoro.remaining.should == 15.minutes
     end
 
-    it "can calculate the remainging time" do
-      Timecop.travel(Time.now + 25.minutes)
+    it "works if all time has elapsed" do
+      Timecop.travel(25.minutes)
       pomodoro.remaining.should == 0
     end
 
-    it "can calculate the remainging time" do
-      Timecop.travel(Time.now + 30.minutes)
+    it "works if more time has elapsed" do
+      Timecop.travel(30.minutes)
       pomodoro.remaining.should == 0
     end
   end
 
-  # it "is active then the remaining time is > 0"
+  describe "states" do
+    it "is active when time remains" do
+      pomodoro = Pomodoro.new(25.minutes)
+      Timecop.travel(10.minutes)
+      pomodoro.state.should == Pomodoro::States::ACTIVE
+    end
+
+    it "is completed when time has elapsed" do
+      pomodoro = Pomodoro.new(25.minutes)
+      Timecop.travel(25.minutes)
+      pomodoro.state.should == Pomodoro::States::COMPLETED
+    end
+
+    it "is interrupted when interrupted" do
+      pomodoro = Pomodoro.new(25.minutes)
+      pomodoro.interrupt!
+      pomodoro.state.should == Pomodoro::States::INTERRUPTED
+    end
+
+  end
+
+  # it "is active when the remaining time is > 0"
   # it "can be completed"
   # it "can be on-break"
   # it "can be distracted"
