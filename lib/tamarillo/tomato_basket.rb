5a73f3e24da4677ca64d114e0a9f932d791048b9
Tim Uruski <timuruski@gmail.com>
2012-04-19 22:22:08 -0600

    TomatoBasket can now save a file

--- lib/tamarillo/tomato_basket.rb
+++ lib/tamarillo/tomato_basket.rb
@@ -1,3 +1,4 @@
+require 'tamarillo/tomato_file'
 require 'fileutils'
 require 'pathname'
 
@@ -13,13 +14,20 @@ module Tamarillo
 
     def save(tomato)
       tomato_path = @pathname.join(tomato_name(tomato))
-      FileUtils.touch(tomato_path)
+      File.open(tomato_path, 'w') { |f| f << tomato_to_file(tomato) }
     end
 
     protected
 
     def tomato_name(tomato)
       'tomato'
+    end
+
+    def tomato_to_file(tomato)
+      tomato_file = TomatoFile.new(tomato)
+      [tomato_file.time_line,
+       tomato_file.task_line,
+       tomato_file.state_line].join("\n")
     end
 
   end
--- spec/lib/tamarillo/tomato_basket_spec.rb
+++ spec/lib/tamarillo/tomato_basket_spec.rb
@@ -5,28 +5,69 @@ include Tamarillo
 
 describe TomatoBasket do
   include FakeFS::SpecHelpers
-  let(:tamarillo_dir) { '~/.tamarillo' }
-  subject { TomatoBasket.new(tamarillo_dir) }
+  let(:storage_path) { '~/.tamarillo' }
+  subject { TomatoBasket.new(storage_path) }
 
   it "creates the storage directory if it is missing" do
     FakeFS do
-      path = '/tmp'
-      expect { TomatoBasket.new(path) }.
-        to change { File.directory?(path) }
+      expect { TomatoBasket.new(storage_path) }.
+        to change { File.directory?(storage_path) }
     end
   end
 
-  it "can write tomatoes to the filesystem" do
+  describe "writing tomatoes to the filesystem" do
+    it "writes files" do
+      FakeFS do
+        now = Time.now
+        tomato = stub(:started_at => now, :state => :active)
+        tomato_path = Pathname.new(storage_path).join('tomato')
+
+        expect { subject.save(tomato) }.
+          to change { File.exist?(tomato_path) }
+      end
+    end
+
+    describe "tomato file" do
+      let(:storage_path) { Pathname.new('/tmp/tamarillo') }
+      let(:tomato_path) { storage_path.join('tomato') }
+
+      before do
+        now = Time.new(2011,1,1,6,0,0)
+        today = Date.new(2011,1,1)
+        tomato = stub(:started_at => now, :date => today, :state => :active)
+        TomatoBasket.new(storage_path).save(tomato)
+      end
+
+      subject { File.readlines(tomato_path.to_s) }
+
+      specify { subject[0].should == '2011-01-01 06:00:00 -0700' }
+      specify { subject[1].should == "Some task I'm working on" }
+      specify { subject[2].should == 'active' }
+    end
+  end
+
+  it "can read tomatoes from the filesystem" do
+    pending
     FakeFS do
-      now = Time.now
-      tomato = stub(:started_at => now)
+      # insert sample file
       tomato_path = Pathname.new(tamarillo_dir).join('tomato')
-
-      expect { subject.save(tomato) }.
-        to change { File.exist?(tomato_path) }
+      TomatoBasket.load(tomato_path) # should return a tomato
     end
   end
 
-  it "can count how many tomatoes are stored"
+
+  # A sample tomato basket looks like this:
+  # |,tamarillo
+  #   |-config
+  #   |-basket.sqlite
+  #   |-basket
+  #     |-2012
+  #       |-0401
+  #         |-060101
+  #
+
+  # it "can count how many tomatoes are stored"
+  # it "can find tomatoes by week"
+  # it "can find tomatoes by month"
   # something can group them by day and maybe task
 end
