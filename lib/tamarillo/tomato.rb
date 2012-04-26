e37b31a2529e526c915b16fc2190fbd530d9c779
Tim Uruski <timuruski@gmail.com>
2012-04-26 07:48:26 -0600

    Add some more documentation.

--- lib/tamarillo/tomato.rb
+++ lib/tamarillo/tomato.rb
@@ -6,32 +6,42 @@ module Tamarillo
   # A Tomato is a 'pomodoro', it keeps track of the amount of time you
   # have focused on a single task. It can be interrupted or completed.
   class Tomato
+    # Internal: A set of valid states a Tomato can be in.
     module States
       ACTIVE = :active.freeze
       COMPLETED = :completed.freeze
       INTERRUPTED = :interrupted.freeze
     end
 
+    # Public: Returns the length of the Tomato in seconds.
     attr_reader :duration
 
+    # Public: Initializes a new Tomato.
+    # 
+    # duration - The length of the Tomato in seconds.
+    # clock    - A Clock instance to keep track of elapsed time.
     def initialize(duration, clock)
       @duration = duration
       @clock = clock
     end
 
+    # Public: Returns the starting Time of the Tomato.
     def started_at
       @clock.start_time
     end
 
+    # Public: Returns the Date the Tomato was started on.
     def date
       @clock.start_date
     end
 
+    # Public: Returns true if two Tomatoes share a start Time.
     def eql?(other)
       other.started_at == started_at || 
       super(other)
     end
 
+    # Public: Returns the number of seconds until completion.
     def remaining
       return 0 if @interrupted
       
@@ -39,24 +49,33 @@ module Tamarillo
       d > 0 ? d : 0
     end
 
+    # Public: Marks the tomato as interrupted.
+    #
+    # Returns the Tomato.
     def interrupt!
       @interrupted = true
+      self
     end
 
+    # Public: Returns true if the Tomato has not been completed or
+    # interrupted.
     def active?
       States::ACTIVE == state
     end
 
+    # Public: Returns true if the elapsed Time matches the duration.
     def completed?
       States::COMPLETED == state
     end
 
+    # Public: Returns true if the Tomato has been interrupted.
     def interrupted?
       States::INTERRUPTED == state
     end
 
     protected
 
+    # Private: Returns which state the Tomato is in.
     def state
       if @interrupted
         States::INTERRUPTED
