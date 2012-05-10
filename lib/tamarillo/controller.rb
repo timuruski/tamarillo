16c5bcc2c59e491218ec2d31c31bd458b02b4aa2
Tim Uruski <timuruski@gmail.com>
2012-05-10 08:33:44 -0600

    Scaffolding to extract controller

---
+++ lib/tamarillo/controller.rb
@@ -0,0 +1,25 @@
+require 'tamarillo'
+
+module Tamarillo
+  # Public: This is intended to provide an environment for tomatoes to
+  # work from. It integrates the storage, config and monitor into a
+  # single coherant object.
+  class Controller
+    def status(format)
+    end
+
+    def start_new_tomato
+    end
+
+    def interrupt_current_tomato
+    end
+
+    def config
+    end
+
+    def update_config
+    end
+
+  end
+end
+
---
+++ spec/lib/tamarillo/controller_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../../../lib/tamarillo/controller'
+
+describe Tamararillo::Controller do
+  describe "#status" do
+  end
+
+  describe "#start_new_tomato" do
+  end
+
+  describe "#interrupt_current_tomato" do
+  end
+
+  describe "#config" do
+  end
+
+  describe "#update_config" do
+  end
+end
