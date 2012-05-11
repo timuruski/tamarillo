1c4836506fcebbd8f12156eaf05cf228a9c27600
Tim Uruski <timuruski@gmail.com>
2012-05-10 22:02:44 -0600

    Add status method to controller

--- lib/tamarillo/controller.rb
+++ lib/tamarillo/controller.rb
@@ -1,11 +1,32 @@
 require 'tamarillo'
 
 module Tamarillo
+  # Formats for tomatoes
+  module Formats
+    HUMAN = :human.freeze
+    PROMPT = :prompt.freeze
+  end
+
   # Public: This is intended to provide an environment for tomatoes to
   # work from. It integrates the storage, config and monitor into a
   # single coherant object.
   class Controller
+    # Initializes a new controller.
+    def initialize(config, storage)
+      @config = config
+      @storage = storage
+    end
+
+    # Public: Formats and returns the status of the current tomato.
+    # Returns nil if no tomato is found.
     def status(format)
+      return unless tomato = @storage.latest
+
+      case format
+      when Formats::HUMAN then format_approx_time(tomato.remaining)
+      when Formats::PROMPT then format_prompt(tomato)
+      else raise "Invalid format"
+      end
     end
 
     def start_new_tomato
@@ -18,6 +39,23 @@ module Tamarillo
     end
 
     def update_config
+    end
+
+    private
+
+    def format_approx_time(time)
+      minutes = (time / 60).round
+      'About %d minutes' % minutes
+    end
+
+    def format_time(time)
+      minutes = (time / 60).floor
+      seconds = time % 60
+      "%02d:%02d" % [minutes, seconds]
+    end
+
+    def format_prompt(tomato)
+      [format_time(tomato.remaining), tomato.remaining, tomato.duration].join(' ')
     end
 
   end
--- spec/lib/tamarillo/controller_spec.rb
+++ spec/lib/tamarillo/controller_spec.rb
@@ -1,7 +1,27 @@
 require_relative '../../../lib/tamarillo/controller'
 
 describe Tamarillo::Controller do
+  let(:config) { stub }
+  let(:storage) { stub }
+
+  subject { Tamarillo::Controller.new(config, storage) }
+
   describe "#status" do
+    let(:tomato) { stub(:remaining => 1500, :duration => 1500) }
+    let(:storage) { stub(:latest => tomato) }
+
+    it "can return a humanized format" do
+      subject.status(Tamarillo::Formats::HUMAN).should == 'About 25 minutes'
+    end
+
+    it "can return a machine optimized format" do
+      subject.status(Tamarillo::Formats::PROMPT).should == '25:00 1500 1500'
+    end
+
+    it "complains if the format is invalid" do
+      expect { subject.status('invalid') }
+        .should raise_error
+    end
   end
 
   describe "#start_new_tomato" do
