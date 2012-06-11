8ceb6f6c13bcf26bf466e0baad4aa3cb37d5614c
Tim Uruski <timuruski@gmail.com>
2012-06-10 23:11:59 -0600

    Restore lost Speech implementation

--- lib/tamarillo/notification/speech.rb
+++ lib/tamarillo/notification/speech.rb
@@ -1,6 +1,13 @@
 module Tamarillo
   module Notification
     class Speech
+      SPEECH_COMMAND = %Q{say "Tomato complete!"}
+
+      # Public: executes the notification.
+      def call
+        system(SPEECH_COMMAND)
+      end
+
     end
   end
 end
