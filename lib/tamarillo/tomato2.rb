bf8f2064287c9036f38263dc9cccea168137fc5c
Tim Uruski <timuruski@gmail.com>
2012-07-09 21:48:07 -0600

    New Tomato implentation that maintains its start time

---
+++ lib/tamarillo/tomato2.rb
@@ -0,0 +1,106 @@
+require 'date'
+
+module Tamarillo
+  # Public: A unit of work.
+  #
+  # A Tomato is a 'pomodoro', it keeps track of the amount of time you
+  # have focused on a single task. It can be interrupted or completed.
+  class Tomato2
+    # Internal: A set of valid states a Tomato can be in.
+    module States
+      ACTIVE = :active.freeze
+      COMPLETED = :completed.freeze
+      INTERRUPTED = :interrupted.freeze
+    end
+
+    # Public: Gets/Sets the length of the tomato in seconds.
+    attr_accessor :duration
+
+    # Public: Returns the start time.
+    attr_reader :started_at
+
+    # Public: Initializes a new Tomato.
+    #
+    # duration - The length of the Tomato in seconds.
+    # clock    - A Clock instance to keep track of elapsed time.
+    def initialize(started_at, duration, clock = Time)
+      @started_at = started_at
+      @duration = duration
+      @clock = clock
+    end
+
+    # Public: Returns the Date the Tomato was started on.
+    def date
+      @started_at.to_date
+    end
+
+    # Public: Returns true if two Tomatoes share a start Time.
+    def eql?(other)
+      other.started_at == started_at &&
+      other.duration == duration
+    end
+
+    # Public: Returns the number of seconds until completion.
+    def remaining
+      d = @duration - elapsed
+      d > 0 ? d : 0
+    end
+
+    # Public: Returns the approximate number of minutes until
+    # completetion.
+    def approx_minutes_remaining
+      (remaining / 60.0).round.to_i
+    end
+
+    # Public: Returns the number of seconds elapsed since start.
+    def elapsed
+      @clock.elapsed
+    end
+
+    # Public: Marks the tomato as interrupted.
+    #
+    # Returns the Tomato.
+    def interrupt!
+      @interrupted = true
+      self
+    end
+
+    # Public: Returns true if the Tomato has not been completed or
+    # interrupted.
+    def active?
+      States::ACTIVE == state
+    end
+
+    # Public: Returns true if the elapsed Time matches the duration.
+    def completed?
+      States::COMPLETED == state
+    end
+
+    # Public: Returns true if the Tomato has been interrupted.
+    def interrupted?
+      States::INTERRUPTED == state
+    end
+
+    # Public: Returns which state the Tomato is in.
+    #
+    # I'd rather keep this internal, but the storage system needs to
+    # know what state the tamato was in when it was written.
+    def state
+      if @interrupted
+        States::INTERRUPTED
+      elsif remaining == 0
+        States::COMPLETED
+      else
+        States::ACTIVE
+      end
+    end
+
+    protected
+
+    # Internal: Returns the numbers of seconds since the start_time
+    def elapsed
+      @clock.now.to_i - @started_at.to_i
+    end
+
+  end
+end
---
+++ spec/lib/tamarillo/tomato2_spec.rb
@@ -0,0 +1,132 @@
+require 'active_support/core_ext/numeric/time'
+require 'tamarillo/tomato2'
+# require_relative '../../../lib/tamarillo/tomato'
+
+include Tamarillo
+
+describe Tomato2 do
+  let(:now) { Time.new(2012,1,1,6,0,0) }
+  subject { Tomato2.new(now, 25.minutes, clock) }
+
+  describe "basic attibutes" do
+    let(:clock) { stub(now: now + 5.minutes) }
+
+    its(:duration) { should == 25.minutes }
+    its(:started_at) { should == now }
+    its(:date) { should == Date.new(2012,1,1) }
+    its(:elapsed) { should == 5.minutes }
+
+    it "update the duration" do
+      subject.duration = 5.minutes
+      subject.duration.should == 5.minutes
+    end
+  end
+
+  describe "comparison" do
+    it "is the same if the start-times match" do
+      time_a = Time.new(2012,1,1,6,0,0)
+      time_b = Time.new(2012,1,1,6,0,0)
+
+      tomato_a = Tomato2.new(time_a, 25.minutes)
+      tomato_b = Tomato2.new(time_b, 25.minutes)
+
+      tomato_a.should eql(tomato_b)
+    end
+
+    it "is not the same if the duration differs" do
+      time_a = Time.new(2012,1,1,6,0,0)
+      time_b = Time.new(2012,1,1,6,0,0)
+
+      tomato_a = Tomato2.new(time_a, 15.minutes)
+      tomato_b = Tomato2.new(time_b, 25.minutes)
+
+      tomato_a.should_not eql(tomato_b)
+    end
+  end
+
+  describe "remaining time" do
+    let(:clock) { stub(now: now + elapsed) }
+    let(:elapsed) { 0 }
+
+    context "when no time has elapsed" do
+      let(:elapsed) { 0 }
+      its(:remaining) { should == 25.minutes }
+    end
+
+    context "if some time has elapsed" do
+      let(:elapsed) { 10.minutes }
+      its(:remaining) { should == 15.minutes }
+    end
+
+    context "when all time has elapsed" do
+      let(:elapsed) { 25.minutes }
+      its(:remaining) { should == 0 }
+    end
+
+    context "when excessive time has elapsed" do
+      let(:elapsed) { 30.minutes }
+      its(:remaining) { should == 0 }
+    end
+
+    context "when it has been interrupted" do
+      before do
+        subject.interrupt!
+      end
+
+      its(:remaining) { should == 25.minutes }
+    end
+
+  end
+
+  describe "approximate time remaining" do
+    let(:clock) { stub(now: now + elapsed) }
+    subject { Tomato2.new(now, 10.minutes, clock) }
+
+    context "when more than 30 seconds between minutes" do
+      let(:elapsed) { 5.minutes + 10.seconds }
+      its(:approx_minutes_remaining) { should == 5 }
+    end
+
+    context "when less than 30 seconds between minutes" do
+      let(:elapsed) { 5.minutes + 50.seconds }
+      its(:approx_minutes_remaining) { should == 4 }
+    end
+  end
+
+  describe "states" do
+    let(:clock) { stub(now: now + elapsed) }
+    let(:elapsed) { 0 }
+
+    context "when just started" do
+      let(:elapsed) { 0 }
+      it { should be_active }
+      it { should_not be_completed }
+      it { should_not be_interrupted }
+    end
+
+    context "when time remains" do
+      let(:elapsed) { 15.minutes }
+      it { should be_active }
+      it { should_not be_completed }
+      it { should_not be_interrupted }
+    end
+
+    context "when time has elapsed" do
+      let(:elapsed) { 25.minutes }
+      it { should_not be_active }
+      it { should be_completed }
+      it { should_not be_interrupted }
+    end
+
+    context "when it has been interrupted" do
+      before do
+        subject.interrupt!
+      end
+
+      it { should be_interrupted }
+      it { should_not be_completed }
+    end
+
+  end
+
+end
